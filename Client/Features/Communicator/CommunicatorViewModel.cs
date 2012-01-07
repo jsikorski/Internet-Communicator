11473b37aa294049512a47d87782e2ecb7e0b097
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-01-07 22:35:00 +0100

    Ribbon dlls

--- Client/Features/Communicator/CommunicatorViewModel.cs
+++ Client/Features/Communicator/CommunicatorViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace Client.Features.Communicator
+﻿using System.Collections.Generic;
+using Caliburn.Micro;
+using Client.Services;
+using Common.Contacts;
+using System.Linq;
+
+namespace Client.Features.Communicator
 {
     public class CommunicatorViewModel
     {
+        public BindableCollection<Contact> Contacts { get; set; }
+        public Contact SelectedContact { get; set; }
+
+        public IList<Contact> Contactses { get; set; }
+
+        private readonly IServerConnection _serverConnection;
+
+        public CommunicatorViewModel(
+            IServerConnection serverConnection)
+        {
+            _serverConnection = serverConnection;
+
+            Contactses = new List<Contact>() { new Contact { Number = 5 }, new Contact { Number = 7 } };
+            Contacts = new BindableCollection<Contact>(Contactses);
+        }
+
+        public void RemoveContact()
+        {
+            Contacts.Remove(SelectedContact);
+            //Contacts.Remove(SelectedContact);
+        }
     }
 }
---
+++ Common/Contacts/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.Contacts
+{
+    public class Contact
+    {
+        public int Number { get; set; }
+    }
+}
