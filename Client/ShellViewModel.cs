d35b4814709582e12c6a0d766298712e3998f3ad
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-01-07 18:21:22 +0100

    Problems with files

--- Client/ShellViewModel.cs
+++ Client/ShellViewModel.cs
@@ -1,72 +1,16 @@
 ﻿using System;
 using System.Windows;
 using Caliburn.Micro;
-using Client.Features.Communicator;
-using Client.Features.Login;
-using Client.Services;
-using Client.Validators;
+using Client.Features.Connection;
+
 namespace Client
 {
-    public class ShellViewModel : Screen, IShell
+    public class ShellViewModel : Conductor<IScreen>.Collection.OneActive, IShell
     {
-        private string _serverAddress;
-        public string ServerAddress
-        {
-            get { return _serverAddress; }
-            set
-            {
-                _serverAddress = value;
-                NotifyOfPropertyChange(() => CanConnect);
-            }
-        }
-
-        public bool CanConnect
-        {
-            get
-            {
-                return _addressValidator.IsValid(ServerAddress);
-            }
-        }
-
-        private readonly IValidator _addressValidator;
-        private readonly IServerConnection _serverConnection;
-        private readonly IWindowManager _windowManager;
-        private readonly LoginViewModel _loginViewModel;
-        private readonly CommunicatorViewModel _communicatorViewModel;
-
-        public ShellViewModel(
-            IValidator addressValidator,
-            IServerConnection serverConnection,
-            IWindowManager windowManager,
-            LoginViewModel loginViewModel,
-            CommunicatorViewModel communicatorViewModel)
+        public ShellViewModel(ConnectionViewModel connectionViewModel)
         {
             base.DisplayName = "Internet communicator";
-
-            _addressValidator = addressValidator;
-            _serverConnection = serverConnection;
-            _windowManager = windowManager;
-            _loginViewModel = loginViewModel;
-            _communicatorViewModel = communicatorViewModel;
-        }
-
-        public void Connect()
-        {
-            _windowManager.ShowWindow(_communicatorViewModel);
-            return;
-
-            try
-            {
-                _serverConnection.Connect(ServerAddress);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Cannot connect to server.", "Connection error");
-                return;
-            }
-
-            _windowManager.ShowWindow(_loginViewModel);
-            TryClose();
+            base.ActivateItem(connectionViewModel);
         }
     }
 }
