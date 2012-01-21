b3c686db4477ea8b8777eb40a3d27ca37622fc67
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-01-21 20:20:30 +0100

    Names mistake fix

--- Protocol/Messages/ConferencialMessageRequest.cs
+++ Protocol/Messages/ConferencialMessageRequest.cs
@@ -6,13 +6,13 @@ using System.Text;
 namespace Protocol.Messages
 {
     [Serializable]
-    public class ConferenceMessageRequest : IRequest
+    public class ConferencialMessageRequest : IRequest
     {
         public int Sender { get; private set; }
         public IEnumerable<int> ReciversNumbers { get; private set; }
         public string Text { get; private set; }
 
-        public ConferenceMessageRequest(int sender, string text, IEnumerable<int> reciversNumbers)
+        public ConferencialMessageRequest(int sender, string text, IEnumerable<int> reciversNumbers)
         {
             ReciversNumbers = reciversNumbers;
             Text = text;
