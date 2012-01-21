13e08090856e7070fa0ec0c445bf859d0c2fee13
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-01-21 22:02:36 +0100

    Temp

---
+++ Client/Commands/Messages/CloseAllConferencialMessagesWindows.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client.Context;
+
+namespace Client.Commands.Messages
+{
+    public class CloseAllConferencialMessagesWindows : ICommand
+    {
+        private readonly ICurrentContext _currentContext;
+
+        public CloseAllConferencialMessagesWindows(
+            ICurrentContext currentContext)
+        {
+            _currentContext = currentContext;
+        }
+
+        public void Execute()
+        {
+            foreach (var conferencialMessagesViewModel in _currentContext.ConferencialMessagesWindows)
+            {
+                conferencialMessagesViewModel.Value.TryClose();
+            }
+        }
+    }
+}
---
+++ Client/Commands/Messages/CloseAllMessagesWindows.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client.Context;
+
+namespace Client.Commands.Messages
+{
+    public class CloseAllMessagesWindows : ICommand
+    {
+        private readonly ICurrentContext _currentContext;
+
+        public CloseAllMessagesWindows(
+            ICurrentContext currentContext)
+        {
+            _currentContext = currentContext;
+        }
+
+        public void Execute()
+        {
+            foreach (var messagesViewModel in _currentContext.MessagesWindows)
+            {
+                messagesViewModel.Value.TryClose();
+            }
+        }
+    }
+}
